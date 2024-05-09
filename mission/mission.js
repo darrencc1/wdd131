d8d774589701059c82576eb6108dfe1437af27a9
darrencc1 <darrencc@byui.edu>
2024-05-09 12:49:06 -0600

    push

--- mission/mission.js
+++ mission/mission.js
@@ -5,9 +5,9 @@ function changeTheme() {
     const selectedTheme = themeSelector.value;  
     if (selectedTheme === 'dark') {       
         document.body.classList.add('dark');      
-        document.querySelector('.logo').src = 'white-logo.webp';
+        document.getElementById('logo').src = 'byui-logo.webp';
     } else {   
         document.body.classList.remove('dark');
-        document.querySelector('.logo').src = 'blue-logo.webp';
+        document.getElementById('logo').src = 'byui-logo.webp';
     }
 }
